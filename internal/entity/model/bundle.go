663705b35f431ea36ec03348f73d2edde0bb8e7e
Chris Runo <chris.runo@gmail.com>
2025-06-28 22:14:12 -0400

    Reset with gqlgen.

--- internal/entity/model/bundle.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Bundle struct {
-	ID           string `json:"id"`
-	EntityTypeID string `json:"entity_type_id"` // FK to EntityType
-	Name         string `json:"name"`           // e.g., "article"
-	Label        string `json:"label"`          // Human-readable
-}
--- internal/entity/model/type.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Type struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"` // e.g., "content"
-	Description string `json:"description"`
-}
